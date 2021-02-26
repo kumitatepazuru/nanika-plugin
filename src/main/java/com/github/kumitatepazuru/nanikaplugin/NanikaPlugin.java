0ff92f36f7e1a73e7ad21c65f87bf296111d66d6
kumitatepazuru <riku.ueda@outlook.jp>
2021-02-27 08:46:21 +0900

    死亡ログ変更

--- src/main/java/com/github/kumitatepazuru/nanikaplugin/NanikaPlugin.java
+++ src/main/java/com/github/kumitatepazuru/nanikaplugin/NanikaPlugin.java
@@ -18,13 +18,13 @@ import java.util.List;
 import java.util.Random;
 
 public final class NanikaPlugin extends JavaPlugin implements Listener {
-    List<String> youkoso_msg = new ArrayList<>(Arrays.asList("kon(*^__^*)tya","（へ。へ）y","こ(´∀｀*）ん","(/*^^)/ﾊｯﾛ-!!","a(*^。^*)hello////"));
+    List<String> youkoso_msg = new ArrayList<>(Arrays.asList("kon(*^__^*)tya", "（へ。へ）y", "こ(´∀｀*）ん", "(/*^^)/ﾊｯﾛ-!!", "a(*^。^*)hello////"));
 
     @Override
     public void onEnable() {
         getLogger().info("なにかプラグインが有効になりました。");
 
-        getServer().getPluginManager().registerEvents(this,this);
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -33,28 +33,28 @@ public final class NanikaPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event){
+    public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         int index = new Random().nextInt(youkoso_msg.size());
         String result = youkoso_msg.get(index);
-        getServer().broadcastMessage("§2§o"+result+"§r§l "+player.getName());
-        player.sendTitle("§9なにかサーバー！","§oPowered by Riku Ueda",20,40,20);
-        Bukkit.dispatchCommand(player, "cp menu item "+player.getName());
+        getServer().broadcastMessage("§2§o" + result + "§r§l " + player.getName());
+        player.sendTitle("§9なにかサーバー！", "§oPowered by Riku Ueda", 20, 40, 20);
+        Bukkit.dispatchCommand(player, "cp menu item " + player.getName());
     }
 
     @EventHandler
-    public void OnDeath(PlayerDeathEvent event){
+    public void OnDeath(PlayerDeathEvent event) {
         Player player = event.getEntity().getPlayer();
         assert player != null;
         Location pos = player.getLocation();
-        player.sendMessage("§c§oあなたは死にました。§r\n§f§l死亡場所:[ X:"+pos.getBlockX()+" Y:"+pos.getBlockY()+" Z:"+pos.getBlockZ()+" ]");
+        getServer().broadcastMessage("§c§o" + player.getName() + "が死にました。§r\n§f§l死亡場所:[ X:" + pos.getBlockX() + " Y:" + pos.getBlockY() + " Z:" + pos.getBlockZ() + " ]");
         BukkitTask task = new BukkitRunnable() {
             @Override
             public void run() {
                 player.getWorld().spawnParticle(
                         Particle.END_ROD,
                         pos,
-                        1000,
+                        100,
                         0.1,
                         255,
                         0.1,
@@ -62,6 +62,6 @@ public final class NanikaPlugin extends JavaPlugin implements Listener {
                 );
             }
         }.runTaskTimer(this, 0, 1L);
-        Bukkit.getServer().getScheduler().runTaskLater(this, task::cancel,100L);
+        Bukkit.getServer().getScheduler().runTaskLater(this, task::cancel, 6000L);
     }
 }
